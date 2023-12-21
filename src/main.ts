8bb1ca3e12588c4abd4b3f0727302c0b1c825cf8
Sean Enck <enckse@voidedtech.com>
2023-12-21 13:25:00 -0500

    fixup command mappings

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,25 @@ import { oclone } from "./git-oclone.ts";
 import { sync } from "./sys-update.ts";
 import { lockbox } from "./lb.ts";
 
-const transcode_media_command = "transcode-media";
-const git_oclone_command = "git-oclone";
-const git_uncommitted_command = "git-uncommitted";
-const sys_update_command = "sys-update";
+type commandable = (args: Array<string>) => void;
+
+const commands: Map<string, commandable> = new Map<string, commandable>();
+commands.set(
+  "transcode-media",
+  (_: Array<string>) => {
+    transcode();
+  },
+);
+commands.set(
+  "git-oclone",
+  oclone,
+);
+commands.set("git-uncommitted", uncommit);
+commands.set("lb", lockbox);
+commands.set("sys-update", (_: Array<string>) => {
+  sync();
+});
 const executable = "utility-wrapper";
-const lb_command = "lb";
 
 function main() {
   if (Deno.args.length === 0) {
@@ -32,37 +45,19 @@ function main() {
   if (command === "--version") {
     version();
   }
+  const cmd = commands.get(command);
+  if (cmd !== undefined) {
+    cmd(args);
+    return;
+  }
   switch (command) {
-    case sys_update_command:
-      sync();
-      break;
-    case transcode_media_command:
-      transcode();
-      break;
-    case git_uncommitted_command:
-      uncommit(args);
-      break;
-    case git_oclone_command:
-      oclone(args);
-      break;
-    case lb_command:
-      lockbox(args);
-      break;
     case "generate": {
       if (args.length !== 1) {
         console.log("target required");
         Deno.exit(1);
       }
       const target = args[0];
-      for (
-        const command of [
-          transcode_media_command,
-          git_oclone_command,
-          git_uncommitted_command,
-          sys_update_command,
-          lb_command,
-        ]
-      ) {
+      for (const command of commands.keys()) {
         Deno.writeTextFileSync(
           join(target, command),
           `#!/usr/bin/env bash\nexec ${executable} ${command} $@`,
